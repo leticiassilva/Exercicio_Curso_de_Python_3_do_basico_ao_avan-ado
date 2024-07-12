c2c3bef85bec4486029b7fdccda2431dd3721975
Leticia <leticia.ssilva.rj@gmail.com>
2024-07-12 16:39:01 -0300

    Treinamento pedindo dados do usuário

--- exercicio2.py
+++ exercicio2.py
@@ -1,8 +1,18 @@
-nome = 'Leticia'
-altura = 1.64
-peso = 52.9
-imc = peso / altura ** 2
-print(imc)
+nome = ""
+altura = 0.0
+peso = 0.0
+imc = 0.0
 
-print(nome, 'tem', altura, 'de', 'altura,', 'pesa',
-      peso, 'quilos', 'e seu IMC é', imc)
+nome = input("Digite seu nome: ")
+print(nome)
+altura = float(input("Digite sua altura: "))
+print(altura)
+peso = float(input("Digite seu peso: "))
+print(peso)
+imc = peso / altura ** 2
+
+linha = f'{nome} tem {altura:.2f} de altura, pesa {peso} quilos e seu IMC é {imc:.2f}'
+print(linha)
+
+#print(nome, 'tem', altura, 'de', 'altura,', 'pesa',
+#      peso, 'quilos', 'e seu IMC é', imc)
