17cd8a517f9d9f1bb48c2b13c0b23e130eabcdb6
Leticia <leticia.ssilva.rj@gmail.com>
2024-07-12 12:15:26 -0300

    Calcular o IMC

---
+++ exercicio2.py
@@ -0,0 +1,8 @@
+nome = 'Leticia'
+altura = 1.64
+peso = 52.9
+imc = peso / altura ** 2
+print(imc)
+
+print(nome, 'tem', altura, 'de', 'altura,', 'pesa',
+      peso, 'quilos', 'e', 'seu', 'IMC', 'é', imc)
